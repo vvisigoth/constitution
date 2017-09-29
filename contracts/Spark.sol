8db28550919e018d3b17beaa53468dff80ceb25b
Fang <fang@fang.io>
2017-09-29 18:52:45 +0200

    Now actually burning sparks after they've been used to claim a star.

--- contracts/Spark.sol
+++ contracts/Spark.sol
@@ -1,10 +1,10 @@
 pragma solidity 0.4.15;
 
 import 'zeppelin-solidity/contracts/token/MintableToken.sol';
+import 'zeppelin-solidity/contracts/token/BurnableToken.sol';
 
-contract Spark is MintableToken {
+contract Spark is MintableToken, BurnableToken {
 	string public name = "Spark";
   string public symbol = "USP";
-  uint public totalSupply = 65280;
   uint public decimals = 18;
 }
