caed7ea6a445aee2550eb37df328a971e4eb6093
Fang <fang@fang.io>
2017-11-01 00:34:45 +0100

    Can now give a single Ethereum address permission to transfer ownership of a ship. Pool uses this to also accept deposits of unlocked stars.

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -30,16 +30,40 @@ contract Pool is MintableToken, BurnableToken
     ships = _ships;
   }
 
-  // give one latent star to the pool.
-  // this contract's address must be set as a launcher for the star's parent.
+  // give one star to the pool.
+  // either of the following requirements must be fulfilled:
+  // 1. the star must be latent, the sender the owner of its parent galaxy, and
+  //    this pool configured as a launcher for that galaxy.
+  // 2. the star must be unlocked, the sender the owner of the star, and this
+  //    pool configured as the transferrer for that star.
   function deposit(uint16 _star)
     external
     isStar(_star)
   {
-    // only a star's parent may deposit it.
-    require(ships.isPilot(ships.getOriginalParent(_star), msg.sender));
-    // attempt to grant the star to us.
-    Constitution(ships.owner()).launch(_star, this, 0);
+    // there are two possible ways to deposit a star:
+    // 1: for latent stars, grant the pool launch permission on a galaxy.
+    //    the pool will launch the deposited star directly to itself.
+    if (ships.isPilot(ships.getOriginalParent(_star), msg.sender)
+        && ships.isLauncher(_star, this))
+    {
+      // attempt to launch the star to us.
+      Constitution(ships.owner()).launch(_star, this, 0);
+    }
+    // 2: for locked stars, grant the pool permission to transfer ownership of
+    //    that star. the pool will transfer the deposited star to itself.
+    else if (ships.isPilot(_star, msg.sender)
+             && ships.isTransferrer(_star, this))
+    {
+      // only accept stars that aren't alive, that are reputationless, "clean".
+      require(!ships.isState(_star, Ships.State.Living));
+      // attempt to transfer the star to us.
+      Constitution(ships.owner()).transferShip(_star, this, true);
+    }
+    // if neither of those are possible, error out.
+    else
+    {
+      revert();
+    }
     // we succeeded, so grant the sender their token.
     mint(msg.sender, oneStar);
   }
@@ -52,7 +76,7 @@ contract Pool is MintableToken, BurnableToken
   {
     // attempt to take one token from them.
     // we use this.call for token operations so that msg.sender gets set
-    // to us. token operations won't complete correctly otherwise.
+    // to us. token operations won't run correctly otherwise.
     this.call.gas(50000)(bytes4(sha3("transferFrom(address,address,uint256)")), msg.sender, this, oneStar);
     // attempt to transfer the sender their star.
     Constitution(ships.owner()).transferShip(_star, msg.sender, true);
